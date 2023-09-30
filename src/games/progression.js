cdf80ad1033942a5e0f0dc972ed23e65c88aad29
obsidinan <obsidinan@gmail.com>
2023-09-30 19:47:08 +0300

    fix progression

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,21 +3,26 @@ import runEngine from '../index.js';
 
 const rules = 'What number is missing in the progression?';
 
-const generateRound = () => {
+const generateProgression = () => {
   const startOfProgression = getRandomInRange(1, 10);
   const arrayOfProgression = [startOfProgression];
   const lengthOfProgression = getRandomInRange(5, 15);
   const stepOfProgression = getRandomInRange(2, 5);
-  const indexOfHiddenNumber = getRandomInRange(0, lengthOfProgression - 1);
 
   for (let i = 1; i < lengthOfProgression; i += 1) {
     arrayOfProgression[i] = arrayOfProgression[i - 1] + stepOfProgression;
   }
 
-  const answer = String(arrayOfProgression[indexOfHiddenNumber]);
-  arrayOfProgression[indexOfHiddenNumber] = '..';
+  return arrayOfProgression;
+};
 
-  const question = arrayOfProgression.join(' ');
+const generateRound = () => {
+  const progression = generateProgression();
+  const indexOfHiddenNumber = getRandomInRange(0, (progression.length - 1));
+
+  const answer = String(progression[indexOfHiddenNumber]);
+  progression[indexOfHiddenNumber] = '..';
+  const question = progression.join(' ');
 
   return [question, answer];
 };
